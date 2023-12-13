0cbfe0048eadd3962b1cd49a7cc26e7da74aedd6
azertyop-e <83665623+azertyop-e@users.noreply.github.com>
2023-12-13 14:21:52 +0100

    tests pwd v2

--- src/test/java/com/iut/banque/test/dao/TestsCreerUtilisateur.java
+++ src/test/java/com/iut/banque/test/dao/TestsCreerUtilisateur.java
@@ -2,6 +2,8 @@ package com.iut.banque.test.dao;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
 import com.iut.banque.controller.CreerUtilisateur;
+import com.iut.banque.modele.Client;
+import com.iut.banque.modele.Utilisateur;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,33 +11,19 @@ import static org.junit.Assert.*;
 
 public class TestsCreerUtilisateur {
 
+    private final CreerUtilisateur creerUtilisateur = new CreerUtilisateur();
+
     /**
      * Tests de la fonction de cryptage de mot de passe
      */
-
-
-    private CreerUtilisateur creerUtilisateur;
-
-    @Before
-    public void setUp() {
-        creerUtilisateur = new CreerUtilisateur();
-    }
-
     @Test
     public void testHashPassword() {
         String plainPassword = "secretPassword";
         try {
             String hashedPassword = creerUtilisateur.hashPassword(plainPassword);
             assertNotEquals(plainPassword, hashedPassword);
-            assertTrue(BCrypt.verifyer().verify(plainPassword.toCharArray(), hashedPassword).verified);
-
         } catch (IllegalArgumentException e) {
             fail("Exception thrown: " + e.getMessage());
         }
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testHashPasswordWithInvalidInput() {
-        creerUtilisateur.hashPassword(null);
     }
 }
